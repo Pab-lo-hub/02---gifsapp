be8b021cea781bb413fe9975c511a4dd2f963c92
pab-lo-hub <salaberry.pablo@gmail.com>
2023-02-05 16:03:55 -0300

    now thew app can search and display the gif images

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = 'mzzcZfioZ6dDyoWlBVpR2m49Bco5AE5T';
   private _historial: string[] = [];
-  
+
+  public resultados: any[] = [];
+
   get historial() {
+
     return [...this._historial];
   }
 
-  buscarGifs( query: string) {
-    this._historial.unshift(query);
-    console.log(this._historial);
+  constructor(private http: HttpClient) { }
+
+  buscarGifs(query: string) {
+    query = query.trim().toLocaleLowerCase()
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+    }
+    this._historial = this._historial.splice(0, 10);
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=mzzcZfioZ6dDyoWlBVpR2m49Bco5AE5T&q=${ query }&limit=10`)
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
+
   }
-  
+
 }
